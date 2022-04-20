5bfb4741da8b8b0765c982721141061417690bb7
Devansh Dwivedi <104082565+devika169@users.noreply.github.com>
2022-04-20 20:05:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 input("Hey Geeks!! Welcome To The Movie Bot, I Will Give You Preferences Of Movies In Diffrent Genre You Can Watch!!, Press Enter To Continue:")
-input("Genres Are 1. Comedy,  2. Horror, 3. Action, 4.Scifi.. Please Use The Same Letters As Above, Because The Bot Is Case Sensitive: ")
+input("Genres Are 1. Comedy,  2. Horror, 3. Action, 4. Scifi, 5. Children. Press Enter To Continue:")
 movie = input("What Genre Of Movies Would You Like To Watch: ")
 
 if movie == "1":
-    print("Pagalpanti, Chup Chup Ke, Hera Pheri, Phir Hera Pheri, Andaz Apna Apna, Dhol, Munaa Bhai M.B.B.S, Mr. Bean, Hungama, Hungama 2, Dhamaal, Total Dhamaal, Golmaal, Golmaal Returns, Veery Ki Wedding.")
+    print("Pagalpanti, Chup Chup Ke, Hera Pheri, Phir Hera Pheri, Andaz Apna Apna, Dhol, Munaa Bhai M.B.B.S, Mr. Bean, Hungama, Hungama 2, Dhamaal, Total Dhamaal, Golmaal, Golmaal Returns, Veery Ki Wedding, F.R.I.E.N.D.S")
 
 elif movie == "2":
-    print("Anabella, I.T, Kanchana, Kanchana 2, The Final Destination 1-2-3-4, The Nun, Laxmii.")
+    print("Anabella, I.T, Kanchana, Kanchana 2, The Final Destination 1-2-3-4, The Nun, Laxmii, Chhorii, Stree, Cojuring Series, Roohi")
 
 elif movie == "3":
-    print("Singham, Singham 2, Singham Returns, Simba, Dabang 1-2-3, Baby, Naam Shabana, Special 26, Dhoom 1-2-3, Sooryavanshi, Mai Hoo Na")
+    print("Singham, Singham 2, Singham Returns, Simba, Baby, Dhoom 1-2-3, Sooryavanshi, KGF Chapter 1, KGF Chapter 2, Pushpa: The Rise, Don Series, Jurassic Park, Jurassic World, Harry Potter - Series, Bell Bottom, Man V/S Wild ")
 
 elif movie == "4":
-    print("Ra One, Joker, Koi Mil Gaya, Krish - Series, Marvel Series, DC Series")
-
-elif movie == "4":
-    print("Tiger Zinda Hai, Mohenjo Daro, Jurassic Park, Jurassic World, Harry Potter - Series, Bell Bottom, Man V/S Wild")
+    print("Ra One, Joker, Koi Mil Gaya, Krish - Series, Marvel Series, DC Series, Gravity, Men In Black, Inception, X - Men Series, Star Wars Series, Attack, The Adam Project")
 
 elif movie == "5":
-    
     print("Pepa Pig, Dora The Blind Girl, Boss Baby, Oggy And The Cockroaches, Sponge Bob Square Pants, Ninja Hatori, Doaremon, Keymon Ache, Pokemon, Minecraft Storymode, Mr. Bean, Grizzy The Lemmings, Zic And Sharko, Ben Tennison, ")
-
-
