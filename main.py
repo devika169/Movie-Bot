0e2429495b22640a20fec3fbc4dc69597dae60d5
devansh2007 <65642371+devansh2007@users.noreply.github.com>
2022-04-20 19:36:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+input("Hey Geeks!! Welcome To The Movie Bot, I Will Give You Preferences Of Movies In Diffrent Genre You Can Watch!!, Press Enter To Continue:")
+input("Genres Are 1. Comedy,  2. Horror, 3. Action, 4.Scifi.. Please Use The Same Letters As Above, Because The Bot Is Case Sensitive: ")
+movie = input("What Genre Of Movies Would You Like To Watch: ")
+
+if movie == "1":
+    print("Pagalpanti, Chup Chup Ke, Hera Pheri, Phir Hera Pheri, Andaz Apna Apna, Dhol, Munaa Bhai M.B.B.S, Mr. Bean, Hungama, Hungama 2, Dhamaal, Total Dhamaal, Golmaal, Golmaal Returns, Veery Ki Wedding.")
+
+elif movie == "2":
+    print("Anabella, I.T, Kanchana, Kanchana 2, The Final Destination 1-2-3-4, The Nun, Laxmii.")
+
+elif movie == "3":
+    print("Singham, Singham 2, Singham Returns, Simba, Dabang 1-2-3, Baby, Naam Shabana, Special 26, Dhoom 1-2-3, Sooryavanshi, Mai Hoo Na")
+
+elif movie == "4":
+    print("Ra One, Joker, Koi Mil Gaya, Krish - Series, Marvel Series, DC Series")
+
+elif movie == "4":
+    print("Tiger Zinda Hai, Mohenjo Daro, Jurassic Park, Jurassic World, Harry Potter - Series, Bell Bottom, Man V/S Wild")
+
+elif movie == "5":
+    
+    print("Pepa Pig, Dora The Blind Girl, Boss Baby, Oggy And The Cockroaches, Sponge Bob Square Pants, Ninja Hatori, Doaremon, Keymon Ache, Pokemon, Minecraft Storymode, Mr. Bean, Grizzy The Lemmings, Zic And Sharko, Ben Tennison, ")
+
+
